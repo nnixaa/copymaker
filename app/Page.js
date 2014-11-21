efa70e006dee3390227c091fcd3370d5d44d543a
Dmitry Nehaychik <d.nehaychik@itransition.com>
2014-11-21 16:08:13 +0300

    working prototype

--- app/Page.js
+++ app/Page.js
@@ -6,6 +6,11 @@ define('Page', function (require, jquery) {
         this.title = title;
         this.enabled = false;
 
+        this.editableTags = 'p,h1,h2,h3,h4,h5,strong,span,i,li,a,button,td,th';
+        this.editableBorder = '1px dashed #8e0000';
+        this.editableOutline = 'none';
+        this.editableShadow = '1px 1px 3px #b50707';
+
         this.body = $('body').html();
     }
     Page.prototype = {
@@ -15,11 +20,39 @@ define('Page', function (require, jquery) {
         },
 
         enable: function() {
-            $('body').html('<h1>enabled</h1>');
+            var self = this;
+
+            $('body').on('click', self.editableTags, function() {
+
+                var that = this;
+                setTimeout(function() {
+                    $(that).focus();
+                }, 0);
+
+                var border = $(that).css('border');
+                var shadow = $(that).css('box-shadow');
+                var outline = $(that).css('outline');
+
+                $(that).css('border', self.editableBorder);
+                $(that).css('outline', self.editableOutline);
+                $(that).css('box-shadow', self.editableShadow);
+                $(that).prop('contenteditable', 'true');
+
+                $(that).blur(function() {
+
+                    $(that).css('border', border);
+                    $(that).css('outline', outline);
+                    $(that).css('box-shadow', shadow);
+                    $(that).prop('contenteditable', 'false');
+
+                    return false;
+                });
+                return false;
+            });
         },
 
         disable: function() {
-            $('body').html(this.body);
+            $('body').off('click', this.editableTags);
         }
     };
     return Page;
